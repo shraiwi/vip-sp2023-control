824658fd8859e24e497e34decec4076011940474
123416 <71516264+ethanwsu@users.noreply.github.com>
2023-02-25 00:30:45 -0500

    Update mccli.py

--- mccli.py
+++ mccli.py
@@ -4,10 +4,17 @@ from pyvesc import VESC
 def add(a, b):
 	print(f"{a} + {b} = {a+b}")
 
+def wait(seconds):
+	if seconds == 1:
+		print(f"waiting for one second...")
+	else:
+		print(f"waiting for {seconds} seconds...")
+	time.sleep(seconds)
+
 COMMANDS = {
 	# "command name": (method to be called, 1st arg parser, 2nd arg parser, etc...)
 	"add": (add, str, str),
-	"wait": (time.sleep, float),
+	"wait": (wait, float),
 }
 
 if __name__ == "__main__":
@@ -27,14 +34,14 @@ if __name__ == "__main__":
 
 			for command_name, (fn, *arg_parsers) in COMMANDS.items():
 				if user_command.startswith(command_name):
-					str_args = user_command.removeprefix(command_name).split()
-					if len(str_args) != len(arg_parsers):
-						print(f"expected {len(arg_parsers)} args, got {len(str_args)}!")
+					user_args = user_command.removeprefix(command_name).split()
+					if len(user_args) != len(arg_parsers):
+						print(f"expected {len(arg_parsers)} args, got {len(user_args)}")
 					else:
 						parsed_args = []
 
-						for arg_parser, str_arg in zip(arg_parsers, str_args):
-							parsed_args.append(arg_parser(str_arg))
+						for arg_parser, user_arg in zip(arg_parsers, user_args):
+							parsed_args.append(arg_parser(user_arg))
 
 						fn(*parsed_args)
 
